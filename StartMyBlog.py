d81ce65285396286f809dbee168f31cb43db32fd
zhullyb <5435486@qq.com>
2020-07-16 23:19:41 +0800

    Write a tool with python3 to help me to deal with the top of my blog

---
+++ StartMyBlog.py
@@ -0,0 +1,29 @@
+#!/usr/bin/venv python
+
+"""
+本程序需要导入easygui模块和time模块
+"""
+
+import time
+import easygui
+date = time.strftime("%Y-%m-%d", time.localtime())
+title = easygui.enterbox("请输入文章标题")
+categories = easygui.enterbox("请输入文章分类")
+if categories == "\n"
+    print("警告：你没有输入文章分类")
+tags = easygui.enterbox("请输入文章标签")
+f = open(date+"-"+title+".md",'w')
+f.write('---\n')
+f.write('layout:     post\n')
+f.write('layout:     post\n')
+f.write('title:      '+title+'\n')
+f.write('date:       '+date+'\n')
+f.write('author:     竹林里有冰\n')
+f.write('categories: '+categories+'\n')
+f.write('tags:       '+tags+'\n')
+f.write('---\n')
+f.write('\n')
+f.write('* content\n')
+f.write('{:toc}\n')
+f.write('\n')
+f.close()
