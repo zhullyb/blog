40e1e3f8d81263cf5323a2c4ed363df7e7c963f4
zhullyb <zhullyb@outlook.com>
2021-03-21 16:01:59 +0800

    Sync 20210321-1558

--- _posts/StartMyBlog.sh
+++ _posts/StartMyBlog.sh
@@ -1,7 +1,7 @@
 _date=$(date -u +%Y-%m-%d)
 _url=$(kdialog --inputbox url)
-_title=$(kdialog --inputbox 标题)
-_catagories=$(kdialog --inputbox 分类)
+_title=$(kdialog --inputbox 标题（Shell/Rom/Android/汉化/Linux）)
+_catagories=$(kdialog --inputbox 分类（Android/Fun/Linux/Python/Rom/Rom编译/Windows/大佬对话笔记/网络）)
 _tags=$(kdialog --inputbox 标签)
 touch "$_date-$_url.md"
 echo "---
