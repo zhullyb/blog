58127e8fa119cd9056d0e1759848b635afd5ff01
zhullyb <zhullyb@outlook.com>
2021-04-27 21:52:48 +0800

    Sync 20210427-2152

--- _posts/StartMyBlog.sh
+++ _posts/StartMyBlog.sh
@@ -3,8 +3,6 @@ echo "输入url"
 read _url
 echo "输入文章标题"
 read _title
-echo "输入分类（现存: Shell/Rom/Android/汉化/Linux）"
-read _catagories
 echo "输入标签（现存: Android/Fun/Linux/Python/Rom/Rom编译/Windows/大佬对话笔记/网络/笔记）"
 read _tags
 touch "$_date-$_url.md"
@@ -12,7 +10,6 @@ echo "---
 title:      $_title
 date:       $_date
 author:     竹林里有冰
-categories: $_catagories
 tags:       $_tags
 ---
 
